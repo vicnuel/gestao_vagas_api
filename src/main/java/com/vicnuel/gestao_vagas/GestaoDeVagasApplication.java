5827ab2082da3ed317c97804bb5e86c57b9b2b1a
Vicnuel <78376383+vicnuel@users.noreply.github.com>
2025-05-21 17:33:42 -0300

    Initial commit

---
+++ src/main/java/com/vicnuel/gestao_vagas/GestaoDeVagasApplication.java
@@ -0,0 +1,13 @@
+package com.vicnuel.gestao_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicnuel/gestao_vagas/GestaoDeVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicnuel.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
